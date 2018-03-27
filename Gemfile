3aa58fb36354d30ca5ba564f20f5a97464b48f71
Shih Han <swang05@mail.bbk.ac.uk>
2018-03-27 20:44:03 +0100

    SimpleCov install

--- Gemfile
+++ Gemfile
@@ -4,6 +4,13 @@ ruby '2.5.0'
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-group :development, :test do
+
+group :development do
 	gem "rspec"
+end
+
+group :test do
+  gem 'rspec'
+  gem 'simplecov', require: false
+  gem 'simplecov-console', require: false
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,27 +3,27 @@ require 'oystercard'
 describe Oystercard do
   subject(:a_oystercard) { described_class.new }
 
-    it 'Should create an instance (passed no arguments) with a default balance of 0' do
-      expect(a_oystercard.balance).to eq 0.00
+  it 'Should create an instance (passed no arguments) with a default balance of 0' do
+    expect(a_oystercard.balance).to eq 0.00
+  end
+
+  describe '#top-up' do
+
+    it 'Should allow the user to top up the value on the oyster card by £5' do
+      a_oystercard.top_up(5.00)
+      expect(a_oystercard.balance).to eq 5.00
     end
 
-    describe '#top-up' do
-
-      it 'Should allow the user to top up the value on the oyster card by £5' do
-        a_oystercard.top_up(5.00)
-        expect(a_oystercard.balance).to eq 5.00
-      end
-
-      it 'Should not allow the user to top up the value past the maximum of £90' do
-        a_oystercard.top_up(70)
-        expect { a_oystercard.top_up(21) }.to raise_error "Cannot top up past maximum value of £90"
-      end
+    it 'Should not allow the user to top up the value past the maximum of £90' do
+      a_oystercard.top_up(70)
+      expect { a_oystercard.top_up(21) }.to raise_error "Cannot top up past maximum value of £90"
     end
+  end
 
   subject(:b_oystercard) { described_class.new(5.00) }
 
-    it 'Should create an instance (passed starting balance of 5.00) with 5.00 balance' do
-      expect(b_oystercard.balance).to eq 5.00
-    end
+  it 'Should create an instance (passed starting balance of 5.00) with 5.00 balance' do
+    expect(b_oystercard.balance).to eq 5.00
+  end
 
 end
